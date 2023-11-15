05c0e3a7b7c32265801bfb13f05ef8c4e54dc92c
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2023-11-15 22:31:00 +0500

    Task 1 + some cosmetic fix on Task 0

---
+++ Tyuiu.MalsagovUA.Sprint5.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MalsagovUA.Sprint5.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MalsagovUA.Sprint5.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f00e0920-e456-46a9-ab66-5648dfa60438")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.MalsagovUA.Sprint5.Task1.V10/Program.cs
+++ Tyuiu.MalsagovUA.Sprint5.Task1.V10/Program.cs
@@ -3,37 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-using Tyuiu.MalsagovUA.Sprint5.Task0.V27.Lib;
-namespace Tyuiu.MalsagovUA.Sprint5.Task0.V27
+using Tyuiu.MalsagovUA.Sprint5.Task1.V10.Lib;
+namespace Tyuiu.MalsagovUA.Sprint5.Task1.V10
 {
     class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            int x = 3;
+            int startValue = -5;
+            int stopValue = 5;
             Console.Title = "Спринт #5 | Выполнил: Мальсагов У.А. | АСОиУб-23-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
             Console.WriteLine("* Выполнил: Мальсагов Умар Асланович | АСОиУб-23-2                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Дано выражение. Вычислить его значение при x = 3, результат сохранить   *");
-            Console.WriteLine("* в текстовый файл 'OutPutFileTask0.txt' и вывести на консоль.            *");
-            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("* Дана функция. Вычислить её значения на диапазоне [-5;5] с шагом 1.      *");
+            Console.WriteLine("* Результат сохранить в текстовый файл 'OutPutFileTask0.txt' и вывести    *");
+            Console.WriteLine("* на консоль в таблицу. Значения округлить до двух знаков после запятой.  *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* y(x) = (x - 1)^(3x + 1)                                                 *");
+            Console.WriteLine($"Начало цикла: {startValue}");
+            Console.WriteLine($"Конец цикла: {stopValue}");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            string res = ds.SaveToFileTextData(x);
+            string res = ds.SaveToFileTextData(startValue, stopValue);
             Console.WriteLine($"Файл: {res}");
             Console.WriteLine("Успешно создан!");
             Console.ReadKey();
