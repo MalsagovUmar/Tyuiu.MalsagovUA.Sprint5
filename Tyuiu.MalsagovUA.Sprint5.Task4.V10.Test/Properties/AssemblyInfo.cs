ac1170315c5b8533c9d03a4f2f8870837b42c817
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2023-11-22 16:57:03 +0500

    Task 4

---
+++ Tyuiu.MalsagovUA.Sprint5.Task4.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MalsagovUA.Sprint5.Task4.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MalsagovUA.Sprint5.Task4.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a28d42c6-0e42-4890-9a97-c1d17b4368fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MalsagovUA.Sprint5.Task4.V10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Tyuiu.MalsagovUA.Sprint5.Task4.V10.Lib;
+namespace Tyuiu.MalsagovUA.Sprint5.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V10.txt";
+            Console.Title = "Спринт #5 | Выполнил: Мальсагов У.А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Мальсагов Умар Асланович | АСОиУб-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Дан файл, в котором есть вещественное значение. Прочитать значение из   *");
+            Console.WriteLine("* файла и подставить вместо X в формуле: y = x^3 * 1.2x + 2.              *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" y = x^3 * 1.2x + 2");
+            Console.WriteLine($" Данные находятся в файле: {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
